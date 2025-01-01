3f3985339e465f684cc81987673e56c03ce786c6
KG Labs LLC <kennethdeveloper02@gmail.com>
2025-01-01 03:50:57 -0800

    Fix routing and rendering

--- src/App.js
+++ src/App.js
@@ -2,33 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Services from './components/Services';
-import Partners from './components/Partners';
-import Testimonials from './components/Testimonials';
-import Footer from './components/Footer';
-import ServiceDetail from './components/ServiceDetail';
 import Courses from './pages/Courses';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen">
+      <div className="min-h-screen bg-white">
         <Navbar />
         <Routes>
           <Route path="/" element={
             <>
               <Hero />
-              <Services />
-              <Partners />
-              <Testimonials />
+              <Courses />
             </>
           } />
-          <Route path="/services/:serviceId" element={<ServiceDetail />} />
           <Route path="/courses" element={<Courses />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </div>
