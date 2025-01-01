298c8a0cb0e4b723352e01f4cb5a8b9c501f2d28
KG Labs LLC <kennethdeveloper02@gmail.com>
2025-01-01 03:56:45 -0800

    Fix routing and rendering

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,31 +2,10 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        serif: ['Georgia', 'serif'],
-      },
-      colors: {
-        primary: '#112C4A',
-        secondary: '#61C36C',
-        footer: '#87A1FF',
-      },
-      animation: {
-        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      },
-      blur: {
-        '3xl': '64px',
-      },
-      backdropBlur: {
-        xs: '2px',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 } 
